f71ba9f0b98cbe4646025f90fb4676ae2a44dfb3
Pedro Hernández <pedrothdc@gmail.com>
2021-02-05 20:01:01 +0100

    app bootstrap

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script>
+  import TaskColumn from "./components/TaskColumn.svelte";
+  const columns = ["Backlog", "In Progress", "In review", "Done"];
+</script>
+
+<main>
+  <header>
+    <h1>My trello</h1>
+    <p>Crea tus tareas y guardalas en local</p>
+  </header>
+  <section class="board">
+    {#each columns as column}
+      <TaskColumn name={column} />
+    {/each}
+  </section>
+  <footer>Pedro T. Hernández</footer>
+</main>
+
+<style lang="scss">
+  @import "./sass/variables";
+
+  main {
+    display: grid;
+    grid-template-rows: 200px calc(100vh - 250px) 50px;
+    grid-template-columns: 100%;
+    background: url("images/bg.jpg") center center no-repeat;
+    backdrop-filter: blur(8px);
+    background-size: cover;
+    height: 100vh;
+    overflow:hidden;
+  }
+  header {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: $green;
+    text-transform: uppercase;
+    font-size: 4ch;
+    font-weight: 100;
+  }
+  .board {
+    display: grid;
+    grid-template-columns: 24% 24% 24% 24%;
+    grid-template-rows: 1fr;
+    grid-column-gap: 1em;
+    padding: 1rem;
+    background: $bg-board;
+  }
+  footer {
+    color: $font-white;
+    background: $bg-grey;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/components/TaskColumn.svelte
@@ -0,0 +1,33 @@
+<script>
+  import TaskRow from "./TaskRow.svelte";
+  export let name;
+  console.log(name);
+</script>
+
+<div class="task-col">
+  <div class="task-col__header">
+    <h3>{name}</h3>
+  </div>
+  <div class="task-col__droparea">
+    <TaskRow />
+  </div>
+  <button>
+    Añadir Tarea </button>
+</div>
+
+<style lang="scss">
+  @import "../sass/variables";
+
+  .task-col {
+    padding: 0 2ch 2ch;
+    border: 1px dashed #09f;
+    &__header {
+      color: $green;
+      border-bottom: 1px dashed #333;
+    }
+    &__droparea {
+      background: rgba(51, 51, 51, .75);
+      height: 100%;
+    }
+  }
+</style>
